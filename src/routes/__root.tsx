fa1db8c38297662341681a88068efa7371b279b3
mosh <mosh.ohayo@gmail.com>
2025-12-13 12:57:13 +0900

    feat: 右下のTanstackロゴを削除

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,23 +1,10 @@
 import { Outlet, createRootRoute } from "@tanstack/react-router";
-import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
-import { TanStackDevtools } from "@tanstack/react-devtools";
 import { GameStateProvider } from "../contexts/GameStateContext";
 
 export const Route = createRootRoute({
   component: () => (
     <GameStateProvider>
       <Outlet />
-      <TanStackDevtools
-        config={{
-          position: "bottom-right",
-        }}
-        plugins={[
-          {
-            name: "Tanstack Router",
-            render: <TanStackRouterDevtoolsPanel />,
-          },
-        ]}
-      />
     </GameStateProvider>
   ),
 });
